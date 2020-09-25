f23e7828aca8750cee51492b3a0bbbf10071148a
sasami-street-bb <myui076v@gmail.com>
2020-09-25 23:03:44 +0900

    データ保存のコントローラー記述

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,23 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    # binding.pry
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :email, :password, :fee, :memo).merge(user_id: current_user.id)
   end
 
 end
