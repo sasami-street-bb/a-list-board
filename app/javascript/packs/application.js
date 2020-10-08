1232112b45f35ebbf56a0eff493b46a7a191f5fa
sasami-street-bb <myui076v@gmail.com>
2020-10-08 14:13:31 +0900

    確認ダイアログ表示させる設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,16 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../modal")
 require('jquery')
+require('data-confirm-modal')
+//= require data-confirm-modal
+//= require bootstrap-sprockets
+import 'bootstrap';
+import '../stylesheets/application';
 
 //= require turbolinks
 //= require jquery
